fbc8746b31719f50b0d13d0e6d5f8db0cb4e67f6
panascharoenruk <73679556+panascharoenruk@users.noreply.github.com>
2020-10-30 18:13:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <head> <meta charset="UTF-8"></head>
 <?php 
 echo "<h2> Panas Charoenruk</h2>";
-echo "Potter<br>";
+echo "Potter 63070107";
 echo "<img src="Elmo.gif" />"
 echo '<a href="https://www.it.kmitl.ac.th/th/"><img src="pic1.png" height="200" width="200"></a><br>';
 echo 'I fear the day when people post<br>';
