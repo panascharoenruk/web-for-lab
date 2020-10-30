3886e1f808938e48cc8d157be4a0a9c255e2a896
panascharoenruk <73679556+panascharoenruk@users.noreply.github.com>
2020-10-30 18:21:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <?php 
 echo "<h2> Panas Charoenruk</h2>";
 echo "Potter 63070107<br>";
-echo "<img src="Elmo.gif" height="200" width="200">";
 echo '<a href="https://www.it.kmitl.ac.th/th/"><img src="pic1.png" height="200" width="200"></a><br>';
 echo 'I fear the day when people post<br>';
 echo 'memes with quotes i never said<br>';
