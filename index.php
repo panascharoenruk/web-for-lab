599346f8b6e6eb7cfab21b7f979fc858765d1383
panascharoenruk <73679556+panascharoenruk@users.noreply.github.com>
2020-10-30 10:51:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 echo "<h2> Panas Charoenruk</h2>";
 echo "Potter<br>";
 echo '<img src="Elmo.gif" height="200" width="200">';
-echo '<img src="pic1.png" height="200" width="200">'
+echo '<a href="https://www.it.kmitl.ac.th/th/"><img src="pic1.png" height="200" width="200"></a>';
 ?>
