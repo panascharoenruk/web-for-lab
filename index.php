a7fdc69489f2cbe520052a46eb878d9d00e24cc6
panascharoenruk <73679556+panascharoenruk@users.noreply.github.com>
2020-10-30 10:49:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
-echo "<h2> SAWADDEE ITF </h2>";
-echo "TAN PU JA LERN<br>";
+echo "<h2> Panas Charoenruk</h2>";
+echo "Potter<br>";
 echo '<img src="Elmo.gif" height="200" width="200">';
 echo '<img src="pic1.png" height="200" width="200">'
 ?>
