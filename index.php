c41be2f3a82f69473bff877447a8a98ce94731b9
panascharoenruk <73679556+panascharoenruk@users.noreply.github.com>
2020-11-06 10:56:25 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <div id="mycarousel" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                      <li data-target="#mycarousel" data-slide-to="0" class="active"></li>
+                      <li data-target="#mycarousel" data-slide-to="1"></li>
+                      <li data-target="#mycarousel" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img src="img/creative/creative.jpg" class="d-block img-fluid" alt="...">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="img/creative/creative2.jpg" class="d-block img-fluid" alt="...">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="img/creative/creative3.jpg" class="d-block img-fluid" alt="...">
+                      </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#mycarousel" role="button" data-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#mycarousel" role="button" data-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <img src="img/content/office1.jpg" alt="" style="height: 180; width: 250;">
+                <img src="img/content/office2.jpg" alt="" style="height: 180; width: 250;">
+                <img src="img/content/office4.jpg" alt="" style="height: 180; width: 250;">
+            </div>
+            <div class="col-sm-3">
+                <img src="img/staffs/staff4.jpg" alt="" style="height: 360; width: 250;"><br>
+                <img src="img/content/office3.jpg" alt="" style="height: 180; width: 250;">
+            </div>
+        </div>
+    </div>
+    
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+
+</html>
