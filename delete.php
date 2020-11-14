cd3946b32768651f43eab80c72d3dbc25455c472
panascharoenruk <73679556+panascharoenruk@users.noreply.github.com>
2020-11-15 01:01:48 +0700

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,25 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'michelle23.mysql.database.azure.com', 'panas23@michelle23', 'Michelle23', 'itflab2', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+$id = $_GET['id']; // get id through query string
+
+$del = mysqli_query($db,"delete from tblemp where id = '$id'"); // delete query
+
+if($del)
+{
+    mysqli_close($db); // Close connection
+    header("location:all_records.php"); // redirects to all records page
+    exit;	
+}
+else
+{
+    echo "Error deleting record"; // display error message if not delete
+}
+
+?>
