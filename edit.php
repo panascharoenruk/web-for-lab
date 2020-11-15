7d9815a91d6b86a74d6a83c81dd4fb666bd81865
panascharoenruk <73679556+panascharoenruk@users.noreply.github.com>
2020-11-15 13:21:52 +0700

    Add files via upload

---
+++ edit.php
@@ -0,0 +1,43 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'michelle23.mysql.database.azure.com', 'panas23@michelle23', 'Michelle23', 'itflab2', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$id = $_GET['id']; // get id through query string
+
+$qry = mysqli_query($conn,"select * from newbook where id='$id'"); // select query
+
+$data = mysqli_fetch_array($qry); // fetch data
+
+if(isset($_POST['update'])) // when click on Update button
+{
+    $name = $_POST['Name'];
+    $comment = $_POST['Comment'];
+	
+    $edit = mysqli_query($conn,"update newbook set Name='$name', Comment='$comment' where id='$id'");
+	
+    if($edit)
+    {
+        mysqli_close($conn); // Close connection
+        header("location:show.php"); // redirects to all records page
+        exit;
+    }
+    else
+    {
+        echo mysqli_error();
+    }    	
+}
+?>
+
+<h3>Update Data</h3>
+
+<form method="POST">
+  <input type="text" name="Name" value="<?php echo $data['Name'] ?>" placeholder="Enter Name" Required>
+  <input type="text" name="Comment" value="<?php echo $data['Comment'] ?>" placeholder="Enter Comment" Required>
+  <input type="submit" name="update" value="Update">
+</form>
