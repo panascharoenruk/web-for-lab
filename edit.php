2181d6155b10351b1dd1924cf389ee59dc80daab
panascharoenruk <73679556+panascharoenruk@users.noreply.github.com>
2020-11-17 23:00:39 +0700

    Add files via upload

--- edit.php
+++ edit.php
@@ -38,7 +38,9 @@ if(isset($_POST['update'])) // when click on Update button
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
-<h3>Update Data</h3>
+<nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Update Data</a>
+  </nav>
 
 <form method="POST">
 <div class="container">
